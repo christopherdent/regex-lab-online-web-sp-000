7e4b1be0de0356792a8e95bbff5896c3439c0c9a
Christopher Dent <christopherdent01@gmail.com>
2019-07-14 18:56:24 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,8 +20,8 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match(/[A-Z]/)
-
+if text.match(/[A-Z]/)
+end
 end
 
 def valid_phone_number?(phone)
